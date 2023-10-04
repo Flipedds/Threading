e149f0bc391596d0ac6e11e9b5890c6bc5c37742
Filipe André <flipeandre12@gmail.com>
2023-10-04 17:07:14 -0300

    feat: threading console

---
+++ Threading/Program.cs
@@ -0,0 +1,13 @@
+﻿using Threading.models;
+
+ThreadC thread1 = new("100 a 300", 100, 300);
+ThreadC thread2 = new("301 a 500", 301, 500);
+ThreadC thread3 = new("501 a 700", 501, 700);
+
+Thread execUm = new(thread1.Run);
+Thread execDois = new(thread2.Run);
+Thread execTres = new(thread3.Run);
+
+execUm.Start();
+execDois.Start();
+execTres.Start();
---
+++ Threading/models/ThreadC.cs
@@ -0,0 +1,27 @@
+﻿namespace Threading.models;
+
+public class ThreadC
+{
+    private string Nome;
+    private int Inicio;
+    private int Fim;
+    
+    public ThreadC(string nome, int inicio, int fim)
+    {
+        Nome = nome;
+        Inicio = inicio;
+        Fim = fim;
+    }
+
+
+    public void Run()
+    {
+        for (var i = Inicio; i < Fim; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine($"{Nome} - " + i);
+            }
+        }
+    }
+}
